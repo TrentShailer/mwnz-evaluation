176bbe62c6952ea9579f24248b36aa338feaeeec
Trent Shailer <trent.shailer@gmail.com>
2024-06-21 16:58:07 +1200

    Updated error handling to match spec updates

--- companies-backend/src/api_error.rs
+++ companies-backend/src/api_error.rs
@@ -1,5 +1,9 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
-use serde::Serialize;
+use axum::{
+    async_trait,
+    extract::{path::ErrorKind, rejection::PathRejection, FromRequestParts},
+    http::{request::Parts, StatusCode},
+};
+use serde::{de::DeserializeOwned, Serialize};
 
 #[derive(Debug, Serialize)]
 pub struct ApiError {
@@ -7,9 +11,61 @@ pub struct ApiError {
     pub error_description: String,
 }
 
-impl IntoResponse for ApiError {
-    /// To match spec, all api errors are 404 errors
-    fn into_response(self) -> axum::response::Response {
-        (StatusCode::NOT_FOUND, Json(self)).into_response()
+/// from https://github.com/tokio-rs/axum/blob/main/examples/customize-path-rejection/src/main.rs<br>
+/// Gives Path extractor ApiError body
+pub struct PathExctractor<T>(pub T);
+
+#[async_trait]
+impl<S, T> FromRequestParts<S> for PathExctractor<T>
+where
+    // these trait bounds are copied from `impl FromRequest for axum::extract::path::Path`
+    T: DeserializeOwned + Send,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, axum::Json<ApiError>);
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        match axum::extract::Path::<T>::from_request_parts(parts, state).await {
+            Ok(value) => Ok(Self(value.0)),
+            Err(rejection) => {
+                let (status, body) = match rejection {
+                    PathRejection::FailedToDeserializePathParams(inner) => {
+                        let status = StatusCode::BAD_REQUEST;
+
+                        let kind = inner.into_kind();
+
+                        let body = match &kind {
+                            ErrorKind::Message(msg) => ApiError {
+                                error: "Bad Request".to_string(),
+                                error_description: msg.clone(),
+                            },
+
+                            _ => ApiError {
+                                error: "Bad Request".to_string(),
+                                error_description: kind.to_string(),
+                            },
+                        };
+
+                        (status, body)
+                    }
+                    PathRejection::MissingPathParams(error) => (
+                        StatusCode::BAD_REQUEST,
+                        ApiError {
+                            error: "Bad Request".to_string(),
+                            error_description: error.to_string(),
+                        },
+                    ),
+                    _ => (
+                        StatusCode::BAD_REQUEST,
+                        ApiError {
+                            error: "Bad Request".to_string(),
+                            error_description: String::new(),
+                        },
+                    ),
+                };
+
+                Err((status, axum::Json(body)))
+            }
+        }
     }
 }
--- companies-backend/src/companies_route.rs
+++ companies-backend/src/companies_route.rs
@@ -1,19 +1,21 @@
 use axum::{
-    extract::Path,
     http::StatusCode,
     response::{IntoResponse, Response},
     Json,
 };
 
-use crate::{api_error::ApiError, fetch_company, try_fetch_company, Company};
+use crate::{
+    api_error::{ApiError, PathExctractor},
+    fetch_company, try_fetch_company, Company,
+};
 
-pub async fn get_companies(Path(id): Path<i32>) -> Response {
+pub async fn get_companies(PathExctractor(id): PathExctractor<i32>) -> Response {
     let xml = match try_fetch_company(id)
         .await
         .map_err(|e| fetch_company_to_api_error(e, id))
     {
         Ok(xml) => xml,
-        Err(e) => return e.into_response(),
+        Err(e) => return e,
     };
 
     let company = match Company::try_from_xml(xml) {
@@ -21,43 +23,56 @@ pub async fn get_companies(Path(id): Path<i32>) -> Response {
         Err(e) => {
             log::error!("Failed to parse company:\n{e}");
 
-            return ApiError {
-                error: "Failed to parse company".to_string(),
-                error_description: format!("Could not parse xml for company with id {id}"),
-            }
-            .into_response();
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(ApiError {
+                    error: "Failed to parse company".to_string(),
+                    error_description: format!("Could not parse xml for company with id {id}"),
+                }),
+            )
+                .into_response();
         }
     };
 
     (StatusCode::OK, Json(company)).into_response()
 }
 
-/// Transforms a fetch_company::Error into a ApiError
-fn fetch_company_to_api_error(error: fetch_company::Error, company_id: i32) -> ApiError {
+/// Transforms a fetch_company::Error into an api response
+fn fetch_company_to_api_error(error: fetch_company::Error, company_id: i32) -> Response {
     match error {
-        fetch_company::Error::NotFound(_) => ApiError {
-            error: "Company not found".to_string(),
-            error_description: format!("No company with id {company_id} exists."),
-        },
+        fetch_company::Error::NotFound(_) => (
+            StatusCode::NOT_FOUND,
+            Json(ApiError {
+                error: "Company not found".to_string(),
+                error_description: format!("No company with id {company_id} exists."),
+            }),
+        ),
 
         fetch_company::Error::StatusCode(code) => {
             log::error!("Failed to fetch company with id {company_id}\n{error}");
-            ApiError {
-                error: "Failed to fetch company".to_string(),
-                error_description: format!(
-                    "Unexpected status code was received when fetching company: {code}."
-                ),
-            }
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(ApiError {
+                    error: "Failed to fetch company".to_string(),
+                    error_description: format!(
+                        "Unexpected status code was received when fetching company: {code}."
+                    ),
+                }),
+            )
         }
 
         fetch_company::Error::Reqwest(_) => {
             log::error!("Failed to fetch company with id {company_id}\n{error}");
-            ApiError {
-                error: "Failed to fetch company".to_string(),
-                error_description: format!(
-                    "Encountered an error while fetching company {company_id}"
-                ),
-            }
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(ApiError {
+                    error: "Failed to fetch company".to_string(),
+                    error_description: format!(
+                        "Encountered an error while fetching company {company_id}"
+                    ),
+                }),
+            )
         }
     }
+    .into_response()
 }
