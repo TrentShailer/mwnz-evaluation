35416bd1da5707e23551fbde975a8e67a125d5ea
Trent Shailer <trent.shailer@gmail.com>
2024-06-21 17:06:03 +1200

    Test for additional tag in xml

--- companies-backend/src/tests/company_type.rs
+++ companies-backend/src/tests/company_type.rs
@@ -24,6 +24,29 @@ mod company_test {
         );
     }
 
+    #[test]
+    fn valid_xml_additional_tag() {
+        let xml = r#"<?xml version="1.0" encoding="UTF-8"?>
+			<Data>
+				<id>1</id>
+				<name>MWNZ</name>
+				<description>..is awesome</description>
+				<location>New Zealand</location>
+			</Data>"#
+            .to_string();
+
+        let company = Company::try_from_xml(xml).unwrap();
+
+        assert_eq!(
+            company,
+            Company {
+                id: 1,
+                name: String::from("MWNZ"),
+                description: String::from("..is awesome")
+            }
+        );
+    }
+
     /// During testing I found that the parser doesn't care about the container name.
     /// This may be a problem should the case below be intended to fail.
     #[test]
